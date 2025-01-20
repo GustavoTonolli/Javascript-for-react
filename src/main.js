0a9d6a6e44f7e5a8b51a7a6560c576794679e766
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2025-01-19 21:16:19 -0300

    feat(1): manipulacao de objetos

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './style.css'
+
+const pessoa = {
+  nome: 'Gustavo',
+  sobrenome: 'Tonolli',
+  apelido: 'Gu',
+  idade: 28,
+  endereco: {
+    rua: 'Joao de Souza Melo',
+    numero: 364,
+    cidade: 'Joinville'
+  }
+}
+
+const dataEntries = JSON.stringify(Object.entries(pessoa))
+
+const dataValues = JSON.stringify(Object.values(pessoa))
+
+const dataKeys = JSON.stringify(Object.keys(pessoa))
+
+const existeNome = "nome" in pessoa
+
+const { nome, sobrenome, idade
+} = pessoa
+
+document.querySelector('#app').innerHTML = JSON.stringify('Meu nome é ' + nome + ' ' + sobrenome + ' e tenho ' + idade + ' anos.')
+
+setupCounter(document.querySelector('#counter'))
